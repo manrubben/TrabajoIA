ef0d06619a76685284602e7dd06bf447de1fb677
Manu <manuel_sfc98@hotmail.com>
2020-06-12 13:09:48 +0200

    commit de prueba

--- clase principal.py
+++ clase principal.py
@@ -10,13 +10,12 @@ titanic = pd.read_csv(titanic_dataset, header=None, names=['Pclass', 'sex', 'age
 
 atributos_titanic = titanic.loc[:, 'Pclass':'Is_Married'] # selección de las columnas de atributos
 objetivo_titanic = titanic['Survived'] # selección de la columna objetivo
-N_EXP = 1
-CV = 3
-print(objetivo_titanic)
+# N_EXP = 1
+# CV = 3
 
-def metodo_evaluacion_robusta(titanic_dataset, atributos_titanic, N_EXP, CV):
+def metodo_evaluacion_robusta(dataset, atributos, N_EXP, CV):
     for i in range(N_EXP):
         scores = model_selection.cross_val_score(estimator=tree, X=atributos_titanic, cv=CV, scoring='balanced_accuracy')
-        print(scores)
+        return scores
 
-print(metodo_evaluacion_robusta)
+metodo_evaluacion_robusta(titanic_dataset, atributos_titanic, 1, 3)
