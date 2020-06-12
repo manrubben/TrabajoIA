d86107ba97ace978cb1f921f81ffbae4c34a96d4
Manu <manuel_sfc98@hotmail.com>
2020-06-12 22:50:16 +0200

    parsear a float hecho. Error con la primera fila

--- clase principal.py
+++ clase principal.py
@@ -6,7 +6,7 @@ from sklearn import preprocessing
 titanic_dataset = 'docs/titanic.csv'
 breast_cancer_dataset = 'docs/breast_cancer_dataset.csv'
 
-titanic = pd.read_csv(titanic_dataset, header=None, names=['Pclass', 'sex', 'age', 'SibSp', 'Parch', 'Fare', 'Embarked', 'Initial', 'Age_band', 
+titanic = pd.read_csv(titanic_dataset, header=None, delimiter=',', names=['Pclass', 'sex', 'age', 'SibSp', 'Parch', 'Fare', 'Embarked', 'Initial', 'Age_band', 
 'Family_size', 'Alone', 'Fare_cat', 'Deck', 'Title', 'Is_Married', 'Survived']) # se lee el csv y se indican el nombre de las columnas
 
 atributos_titanic = titanic.loc[:, 'Pclass':'Is_Married'] # selección de las columnas de atributos
@@ -15,14 +15,22 @@ objetivo_titanic = titanic['Survived'] # selección de la columna objetivo
 # CV = 3
 
 def metodo_evaluacion_robusta(dataset, atributos, N_EXP, CV):
+
+    iterator = dataset.itertuples() # creamos el iterador de tuplas
+    next(iterator) # nos saltamos la primer fila
+    for row in iterator: # recorremos cada fila del csv
+        for e in row: # recorremos cada elemento de la fila
+            float(e) # pasamos el elemento a float
+    
     codificador_atributos = preprocessing.OrdinalEncoder()
     codificador_atributos.fit(atributos)
 
     clasif_arbol_decision = tree.DecisionTreeClassifier()
     clasif_arbol_decision.fit(X=dataset, y=atributos)
-    for i in range(N_EXP):
-        print('===')
-        scores = model_selection.cross_val_score(estimator=clasif_arbol_decision, X=atributos, cv=CV, scoring='balanced_accuracy')
-        return scores
 
-metodo_evaluacion_robusta(titanic_dataset, atributos_titanic, 1, 3)
+    for i in range(N_EXP):
+        print('Iteración: '+i)
+        scores = model_selection.cross_val_score(estimator=clasif_arbol_decision, X=atributos, cv=CV, scoring='balanced_accuracy')
+        print(scores)
+
+metodo_evaluacion_robusta(titanic, atributos_titanic, 1, 3)
--- prueba parsear csv.py
+++ prueba parsear csv.py
@@ -6,5 +6,9 @@ titanic_dataset = 'docs/titanic.csv'
 titanic = pd.read_csv(titanic_dataset, header=None, delimiter=',', names=['Pclass', 'sex', 'age', 'SibSp', 'Parch', 'Fare', 'Embarked', 'Initial', 'Age_band', 
 'Family_size', 'Alone', 'Fare_cat', 'Deck', 'Title', 'Is_Married', 'Survived']) # se lee el csv y se indican el nombre de las columnas
 
-for row in titanic.itertuples():
-    print(row)
+itertitanic = titanic.itertuples()
+next(itertitanic)
+for row in itertitanic:
+    for e in row:
+        print(e)
+
