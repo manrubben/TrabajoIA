f3817caa0bea78a1e4d0d8a447e1db09b4d531c7
Manu <manuel_sfc98@hotmail.com>
2020-06-12 14:37:03 +0200

    codificador y estimator añadidos

--- clase principal.py
+++ clase principal.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn import model_selection
 from sklearn import tree
+from sklearn import preprocessing
 
 titanic_dataset = 'docs/titanic.csv'
 breast_cancer_dataset = 'docs/breast_cancer_dataset.csv'
@@ -14,8 +15,14 @@ objetivo_titanic = titanic['Survived'] # selección de la columna objetivo
 # CV = 3
 
 def metodo_evaluacion_robusta(dataset, atributos, N_EXP, CV):
+    codificador_atributos = preprocessing.OrdinalEncoder()
+    codificador_atributos.fit(atributos)
+
+    clasif_arbol_decision = tree.DecisionTreeClassifier()
+    clasif_arbol_decision.fit(X=dataset, y=atributos)
     for i in range(N_EXP):
-        scores = model_selection.cross_val_score(estimator=tree, X=atributos_titanic, cv=CV, scoring='balanced_accuracy')
+        print('===')
+        scores = model_selection.cross_val_score(estimator=clasif_arbol_decision, X=atributos, cv=CV, scoring='balanced_accuracy')
         return scores
 
 metodo_evaluacion_robusta(titanic_dataset, atributos_titanic, 1, 3)
