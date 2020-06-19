17f9a4bdffdd0f41c252ca51327c22c917cef793
Manu <manuel_sfc98@hotmail.com>
2020-06-19 17:18:54 +0200

    sffs casi terminado

--- clase principal.py
+++ clase principal.py
@@ -23,11 +23,11 @@ def metodo_evaluacion_robusta(dataset, atributos, objetivo, N_EXP, CV):
     codificador_atributos.fit(atributos) 
     atributos_codificados = codificador_atributos.transform(atributos)
     #print('Atributos codificados: ',atributos_codificados)
-    print('Tamaño x: ',atributos_codificados.shape)
+    #print('Tamaño x: ',atributos_codificados.shape)
 
     codificador_objetivo = preprocessing.LabelEncoder() # Codificador adecuado para el objetivo
     objetivo_codificado = codificador_objetivo.fit_transform(objetivo)
-    print('Tamaño y: ',objetivo_codificado.shape)
+    #print('Tamaño y: ',objetivo_codificado.shape)
     
     #print(codificador_objetivo.classes_)  # Clases detectadas por el codificador para la variable objetivo
     #print('Objetivo codificado: ',objetivo_codificado)
@@ -47,11 +47,11 @@ def metodo_evaluacion_robusta(dataset, atributos, objetivo, N_EXP, CV):
     clasif_arbol_decision.fit(X=atributos_entrenamiento, y=objetivo_entrenamiento)
 
     for i in range(N_EXP):
-        print('Iteración: ',i)
+        #print('Iteración: ',i)
         scores = model_selection.cross_val_score(estimator=clasif_arbol_decision, X=atributos_prueba, y=objetivo_prueba, cv=CV, scoring='balanced_accuracy')
-        print('Score: ',scores)
+        #print('Score: ',scores)
         promedio = scores.mean()
-        print('Promedio: ',scores.mean())
+        #print('Promedio: ',scores.mean())
     
     return promedio
 
@@ -101,11 +101,21 @@ def algoritmo_sffs(dataset):
     k=0
 
     variables_predictoras = dataset.columns.tolist()
+    #print('Variables predictoras iniciales: ', variables_predictoras)
+    nombre_objetivo = variables_predictoras.pop(len(variables_predictoras)-1)
+    #print('Variables predictoras sin objetivo: ', variables_predictoras)
+    variables_sin_añadir = dataset.columns.tolist()
+    nombre_objetivo2 = variables_sin_añadir.pop(len(variables_sin_añadir)-1)
+    #print('variables sin añadir: ', variables_sin_añadir)
+    variables_sin_eliminar = dataset.columns.tolist()
+    objetivo = dataset[nombre_objetivo]
     solucion = []
-
-    while !condicion_de_parada: #La condicion de parada es que añadidos contenga todas las variables predictoras
-        i=1
-        for v in variables_predictoras:
+    while not (len(añadidos)==len(variables_predictoras) and k==10): #La condicion de parada es que añadidos contenga todas las variables predictoras
+        i=len(solucion_actual)+1
+        lista_scores = []
+        print('Añadidos: ', añadidos)
+        print('Variables sin añadir: ', variables_sin_añadir)
+        for v in variables_sin_añadir:
             solucion_actual.append(dataset[v])
             solucion_temporal = solucion_actual
             solucion_temporal = np.reshape(np.ravel(solucion_actual), (891,i))
@@ -113,26 +123,45 @@ def algoritmo_sffs(dataset):
             lista_scores.append(score)
             i=i+1
         mejor_promedio = np.amax(lista_scores)
+        print('Mejor promedio: ', mejor_promedio)
         mejor_solucion_temporal = variables_sin_añadir[lista_scores.index(mejor_promedio)]
+        print('Mejor solucion temporal: ', mejor_solucion_temporal)
         #solucion_actual = []
         solucion.append(mejor_solucion_temporal)
         añadidos.append(mejor_solucion_temporal)
-
+        #print('variables predictoras 2: ', variables_predictoras)
+        variables_sin_añadir.remove(variables_sin_añadir[lista_scores.index(mejor_promedio)])
+        #print('variables predictoras 3: ', variables_predictoras)
+        i=i-1
+        
         for v in variables_predictoras: #hay que recorrer todas las variables que no esten en eliminados
-            solucion_actual.remove(dataset[v])
+            print('Variables predictoras: ', variables_predictoras)
+            i=i-1
+            print('Variable: ', v)
+            #print('indice: ', variables_sin_eliminar.index(v))
+            #print('Solucion actual: ', solucion_actual)
+            solucion_actual.remove(solucion_actual[variables_sin_eliminar.index(v)])
+            variables_sin_eliminar.remove(v)
             solucion_temporal = solucion_actual
-            solucion_temporal = np.reshape(np.ravel(solucion_actual), (891,i))
-            score = metodo_evaluacion_robusta(dataset, solucion_temporal, objetivo, 1, 3)
-            lista_scores.append(score)
-            i=i+1
+            #print('Solucion actual: ', solucion_actual)
+            if len(solucion_actual) > 0:
+                solucion_temporal = np.reshape(np.ravel(solucion_actual), (891,i))
+                score = metodo_evaluacion_robusta(dataset, solucion_temporal, objetivo, 1, 3)
+                lista_scores.append(score)
+            
         mejor_promedio2 = np.amax(lista_scores)
         mejor_solucion_temporal2 = variables_sin_añadir[lista_scores.index(mejor_promedio)]
         if mejor_promedio2 > mejor_promedio:
-            solucion_actual.append(mejor_solucion_temporal2)
+            print('======')
+            solucion_actual.append(dataset[mejor_solucion_temporal2])
             eliminados.append(mejor_solucion_temporal)
             mejor_solucion_temporal = mejor_solucion_temporal2
+            k=0
+        else:
+            solucion_actual.append(dataset[mejor_solucion_temporal])
 
-        print('Mejor solucion temporal: ', mejor_solucion_temporal)
+        k=k+1
+        print('Mejor solucion temporal 2: ', mejor_solucion_temporal)
 
         
 
@@ -141,4 +170,5 @@ def algoritmo_sffs(dataset):
     
 
 #metodo_evaluacion_robusta(titanic, atributos_titanic, objetivo_titanic, 1, 10)
-algoritmo_sfs(titanic, 5)
+#algoritmo_sfs(titanic, 5)
+algoritmo_sffs(titanic)
