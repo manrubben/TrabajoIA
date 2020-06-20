e072aa47874e612e682796cf3d7f5db1fcb29bde
Manu <manuel_sfc98@hotmail.com>
2020-06-20 17:37:32 +0200

    commit

--- prueba parsear csv.py
+++ prueba parsear csv.py
@@ -17,62 +17,78 @@ objetivo_titanic = titanic['Survived'] # selección de la columna objetivo
 codificador_atributos = preprocessing.OrdinalEncoder() # Codificador adecuado para los atributos
 codificador_atributos.fit(atributos_titanic) 
 
-variables_predictoras = ['Pclass', 'sex', 'age', 'SibSp', 'Parch', 'Fare', 'Embarked', 'Initial', 'Age_band', 
-                            'Family_size', 'Alone', 'Fare_cat', 'Deck', 'Title', 'Is_Married']
-
-
-#for v in variables_predictoras:
-#    print(v)
-
-#itertitanic = titanic.itertuples()
-#next(itertitanic)
-#for row in itertitanic:
-#    for e in row:
- #       print(e)
 
 
 
-#atributos_entrenamiento, atributos_prueba, objetivo_entrenamiento, objetivo_prueba = model_selection.train_test_split(
-#    atributos_codificados, objetivo_codificado,  # Conjuntos de datos a dividir, usando los mismos índices para ambos
-#    random_state=13465,  # Valor de la semilla aleatoria, para que el muestreo sea reproducible, a pesar de ser aleatorio
-#    test_size=0.2,  # Tamaño del conjunto de prueba
-#    stratify=objetivo_codificado)  # Estratificamos respecto a la distribución de valores en la variable objetivo
+def metodo_evaluacion_robusta(dataset, atributos, objetivo, N_EXP, CV):
 
-   # clasif_arbol_decision = tree.DecisionTreeClassifier()
-   # clasif_arbol_decision.fit(X=atributos_entrenamiento, y=objetivo_entrenamiento)
+    codificador_atributos = preprocessing.OrdinalEncoder() # Codificador adecuado para los atributos
+    codificador_atributos.fit(atributos) 
+    atributos_codificados = codificador_atributos.transform(atributos)
 
-   # for i in range(N_EXP):
-    #    print('Iteración: '+i)
-    #    scores = model_selection.cross_val_score(estimator=clasif_arbol_decision, X=atributos, cv=CV, scoring='balanced_accuracy')
-    #    print(scores)
+    codificador_objetivo = preprocessing.LabelEncoder() # Codificador adecuado para el objetivo
+    objetivo_codificado = codificador_objetivo.fit_transform(objetivo)
 
+    # Dividimos en conjuntos de entrenamiento y prueba los atributos y el objetivo codificado
+    atributos_entrenamiento, atributos_prueba, objetivo_entrenamiento, objetivo_prueba = model_selection.train_test_split(
+    atributos_codificados, objetivo_codificado,  # Conjuntos de datos a dividir, usando los mismos índices para ambos
+    random_state=12345,  # Valor de la semilla aleatoria, para que el muestreo sea reproducible, a pesar de ser aleatorio
+    test_size=.20  # Tamaño del conjunto de prueba
+    )  
 
-#for i in range(10):
-#    print(i)
+    clasif_arbol_decision = tree.DecisionTreeClassifier() # creamos el clasificador
+    clasif_arbol_decision.fit(X=atributos_entrenamiento, y=objetivo_entrenamiento)
+
+    for i in range(N_EXP):
+        #print('Iteración: ',i)
+        scores = model_selection.cross_val_score(estimator=clasif_arbol_decision, X=atributos_prueba, y=objetivo_prueba, cv=CV, scoring='balanced_accuracy')
+        #print('Score: ',scores)
+        promedio = scores.mean()
+        #print('Promedio: ',scores.mean())
+    
+    return promedio
 
 def algoritmo_sfs(dataset, D):
     solucion_actual = []
+    solucion = []
     k=0
     variables_predictoras = dataset.columns.tolist()
-    objetivo = variables_predictoras.pop(len(variables_predictoras)-1)
-    print('Variables predictoras: ', variables_predictoras)
-    print('Objetivo: ', objetivo)
-    #nsamples, nx, ny = dataset.shape
-    #d2_dataset = dataset.reshape((nsamples, nx*ny))
-    #print(atributos)
+    
+    nombre_objetivo = variables_predictoras.pop(len(variables_predictoras)-1)
+    objetivo = dataset[nombre_objetivo]
+    variables_sin_añadir = variables_predictoras
     while k < D:
-        print('Iteracion: ',k)
-        variables_sin_añadir = list(set(variables_predictoras) - set(solucion_actual))
-        print('Solucion actual: ', solucion_actual)
-        print('Variables sin añadir: ',variables_sin_añadir)
+        #variables_sin_añadir = list(set(variables_predictoras) - set(solucion_actual))
+        print('Variables sin añadir: ', variables_sin_añadir)
+        i=1
+        lista_scores = []
         for v in variables_sin_añadir:
-            solucion_actual = np.append(solucion_actual, v, axis=None)
-            #sol = [] 
-            #sol.append(dataset[v])
-            #solucion_actual.append(sol)
-            #solucion_temporal = solucion_actual
-            #print('Solucion actual',solucion_actual)
-            #print('Solucion temporal',solucion_temporal)
-            #scores = metodo_evaluacion_robusta(dataset, solucion_temporal, objetivo, 1, 10)
-            #print(scores)
+            solucion_actual.append(dataset[v])
+            #print('Solucion actual: ', solucion_actual)
+            solucion_temporal = solucion_actual
+            solucion_temporal = np.reshape(np.ravel(solucion_actual), (891,i))
+            #print(solucion_temporal)
+            score = metodo_evaluacion_robusta(dataset, solucion_temporal, objetivo, 1, 3)
+            lista_scores.append(score)
+            i=i+1
+        print('Lista de scores: ',lista_scores)
+        mejor_promedio = np.amax(lista_scores)
+        mejor_solucion_temporal = variables_sin_añadir[lista_scores.index(mejor_promedio)]
+        print('Mejor solucion temporal: ', mejor_solucion_temporal)
+        solucion_actual = []
+        solucion.append(mejor_solucion_temporal)
+        print('Solucion: ', solucion)
+        #solucion_actual.append(variables_sin_añadir[lista_scores.index(mejor_solucion_temporal)])
+        variables_sin_añadir.remove(variables_sin_añadir[lista_scores.index(mejor_promedio)])
+        #print('Solucion actual: ',solucion_actual)
         k=k+1
+
+        print("\nTabla respuesta:\n")
+        for j in range (0,D):
+            frame_data={'solution':solucion, 'score':mejor_promedio}
+
+    df=pd.DataFrame(frame_data)
+
+    return df.sort_values(by=['score'],ascending = False)
+
+algoritmo_sfs(titanic, 5)
--- pruebas.py
+++ pruebas.py
@@ -14,35 +14,120 @@ atributos_titanic = titanic.loc[:, 'Pclass':'Is_Married'] # selección de las co
 objetivo_titanic = titanic['Survived'] # selección de la columna objetivo
 
 
+lista = []
+lista.append('Pclass')
+lista.append('sex')
 
-a = []
-v = 'Pclass'
-h = titanic[v]
-print(h)
-print(h.shape)
-
-a.append(h)
-print(a)
-
-
-codificador_atributos = preprocessing.OrdinalEncoder() # Codificador adecuado para los atributos
-codificador_atributos.fit(a) 
-atributos_codificados = codificador_atributos.transform(a)
-atributos_codificados.reshape(891,1)
-  
-array = h 
-print("Original array : \n", array) 
-  
-# shape array with 2 rows and 4 columns 
-#array2 = np.arange(array).reshape(891, 1) 
-#print("\narray reshaped with 2 rows and 4 columns : \n", array2) 
-
-print(np.reshape(np.ravel(h), (891,1)))
-
-#solucion_temporal = np.reshape(np.ravel(solucion_actual), (891,1)) No borrar
+old_index = lista.index('sex')
+print(old_index)
+lista.insert(0, lista.pop(old_index))
+print(lista)
 
 
-#data = [4,5]
-#data.append([5,6])
-#print(data)
 
+
+
+def metodo_evaluacion_robusta(dataset, atributos, objetivo, N_EXP, CV):
+
+    codificador_atributos = preprocessing.OrdinalEncoder() # Codificador adecuado para los atributos
+    codificador_atributos.fit(atributos) 
+    atributos_codificados = codificador_atributos.transform(atributos)
+
+    codificador_objetivo = preprocessing.LabelEncoder() # Codificador adecuado para el objetivo
+    objetivo_codificado = codificador_objetivo.fit_transform(objetivo)
+
+    # Dividimos en conjuntos de entrenamiento y prueba los atributos y el objetivo codificado
+    atributos_entrenamiento, atributos_prueba, objetivo_entrenamiento, objetivo_prueba = model_selection.train_test_split(
+    atributos_codificados, objetivo_codificado,  # Conjuntos de datos a dividir, usando los mismos índices para ambos
+    random_state=12345,  # Valor de la semilla aleatoria, para que el muestreo sea reproducible, a pesar de ser aleatorio
+    test_size=.20  # Tamaño del conjunto de prueba
+    )  
+
+    clasif_arbol_decision = tree.DecisionTreeClassifier() # creamos el clasificador
+    clasif_arbol_decision.fit(X=atributos_entrenamiento, y=objetivo_entrenamiento)
+
+    for i in range(N_EXP):
+        #print('Iteración: ',i)
+        scores = model_selection.cross_val_score(estimator=clasif_arbol_decision, X=atributos_prueba, y=objetivo_prueba, cv=CV, scoring='balanced_accuracy')
+        #print('Score: ',scores)
+        promedio = scores.mean()
+        #print('Promedio: ',scores.mean())
+    
+    return promedio
+
+def algoritmo_sffs(dataset):
+    solucion_actual = []
+    añadidos = []
+    eliminados = []
+    k=0
+
+    variables_predictoras = dataset.columns.tolist()
+    #print('Variables predictoras iniciales: ', variables_predictoras)
+    nombre_objetivo = variables_predictoras.pop(len(variables_predictoras)-1)
+    #print('Variables predictoras sin objetivo: ', variables_predictoras)
+    variables_sin_añadir = dataset.columns.tolist()
+    nombre_objetivo2 = variables_sin_añadir.pop(len(variables_sin_añadir)-1)
+    #print('variables sin añadir: ', variables_sin_añadir)
+    variables_sin_eliminar = dataset.columns.tolist()
+    nombre_objetivo3 = variables_sin_eliminar.pop(len(variables_sin_eliminar)-1)
+    
+    objetivo = dataset[nombre_objetivo]
+    solucion = []
+    while not (len(añadidos)==len(variables_predictoras) and k==10): #La condicion de parada es que añadidos contenga todas las variables predictoras
+        lista_auxiliar = dataset.columns.tolist()
+        nombre_objetivo_auxiliar = lista_auxiliar.pop(len(variables_sin_eliminar)-1)
+        i=len(solucion_actual)+1
+        lista_scores = []
+        
+        
+        for v in variables_sin_añadir:
+            solucion_actual.append(dataset[v])
+            solucion_temporal = solucion_actual
+            solucion_temporal = np.reshape(np.ravel(solucion_actual), (891,i))
+            score = metodo_evaluacion_robusta(dataset, solucion_temporal, objetivo, 1, 3)
+            lista_scores.append(score)
+            i=i+1
+        mejor_promedio = np.amax(lista_scores)
+        print('Mejor promedio: ', mejor_promedio)
+        mejor_solucion_temporal = variables_sin_añadir[lista_scores.index(mejor_promedio)]
+        print('Mejor solucion temporal: ', mejor_solucion_temporal)
+        
+        solucion.append(mejor_solucion_temporal)
+        añadidos.append(mejor_solucion_temporal)
+        
+        variables_sin_añadir.remove(variables_sin_añadir[lista_scores.index(mejor_promedio)])
+        
+        i=i-1
+        
+        for v in variables_sin_eliminar:
+            i=i-1
+            print('index: ', lista_auxiliar.index(v))
+            solucion_actual.remove(solucion_actual[lista_auxiliar.index(v)])
+            lista_auxiliar.remove(v)
+            solucion_temporal = solucion_actual
+
+            if len(solucion_actual) > 0:
+                solucion_temporal = np.reshape(np.ravel(solucion_actual), (891,i))
+                score = metodo_evaluacion_robusta(dataset, solucion_temporal, objetivo, 1, 3)
+                lista_scores.append(score)
+
+        mejor_promedio2 = np.amax(lista_scores)
+        if mejor_promedio2 > mejor_promedio:
+            solucion_actual.append(dataset[mejor_solucion_temporal2])
+            eliminados.append(mejor_solucion_temporal)
+            mejor_solucion_temporal2 = variables_sin_eliminar[lista_scores.index(mejor_promedio2)]
+            variables_sin_eliminar.remove(mejor_solucion_temporal)
+            mejor_solucion_temporal = mejor_solucion_temporal2
+            k=0
+        else:
+            solucion_actual.append(dataset[mejor_solucion_temporal])
+
+        
+
+
+
+    
+
+#metodo_evaluacion_robusta(titanic, atributos_titanic, objetivo_titanic, 1, 10)
+#algoritmo_sfs(titanic, 5)
+#algoritmo_sffs(titanic)
